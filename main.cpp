fee6ba75e1c52884fc8ba5dbdc3f12e6d7bee8e5
jmkang240 <35613765+jmkang240@users.noreply.github.com>
2018-01-29 17:27:53 -0500

    played playersDeck a pointer and destroyed

--- main.cpp
+++ main.cpp
@@ -8,9 +8,15 @@
 
 int main(void){
 
-    deck playersDeck; //initialize the deck
-    //print the deck
-    std::cout << playersDeck;
-
-    return 0;
+	//initalizes the deck
+    deck *playersDeck; 
+	//dynamically allocates memory for the new deck object
+	playersDeck = new deck();	
+	
+	//print the deck 
+    std::cout << *playersDeck;
+    //deletes the dynamically allocated memory for the new deck object, including what the deck points to
+	delete playersDeck;
+    
+	return 0;
 }
